841222dccc1b357180d4c9b28b2ddf0759f06bdb
SashaLyapanov <lyapanov_s@mail.ru>
2024-10-17 13:16:11 +0300

    Реализация валидации и корректной работы формы Входа и Регистрации

--- src/components/authorization/Login.js
+++ src/components/authorization/Login.js
@@ -2,50 +2,81 @@ import './authorization.css'
 import '../../fonts/roboto/fonts.css'
 import Button from '../button/Button'
 import axios from '../../utils/axios'
-import { useContext } from 'react'
+import {useContext, useState} from 'react'
 import { CustomContext } from '../../utils/Context'
 import { useNavigate } from 'react-router'
+import {useFormik} from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
     
     const {user, setUser} = useContext(CustomContext)
     const navigate = useNavigate()
+    const [error, setError] = useState('');
 
-    const loginUser = (e) => {
-        e.preventDefault()
-        
-        let newUser = {
-            email: e.target[0].value,
-            password: e.target[1].value
-        }
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: Yup.object({
+            password: Yup.string()
+                .required('Поле обязательно для заполнения'),
+            email: Yup.string().email("Поле не удовлетворяет требованиям")
+        }),
+        onSubmit: async values => {
+            let newUser = {
+                email: values.email,
+                password: values.password
+            }
 
             axios.post('auth/signin', newUser)
-            .then(({data}) => 
-            { 
-                setUser({ 
-                    ...data 
-                }) 
-                localStorage.setItem('user', JSON.stringify({ 
-                    ...data 
-                })) 
-                navigate('/') 
-            }) 
-            .catch((err) => console.log(err.message))
-
-    }
+                .then(({data}) =>
+                {
+                    setUser({
+                        ...data
+                    })
+                    localStorage.setItem('user', JSON.stringify({
+                        ...data
+                    }))
+                    navigate('/')
+                })
+                .catch((err) => setError(err))
+        }
+    })
 
     return(
         <div className='authorization'>
             <div className='popup'>
-                <form className='form' onSubmit={loginUser}>
+                <form className='form' onSubmit={formik.handleSubmit}>
                     <p className='header fonts-roboto-black'>Авторизация</p>
-                    <input type='text' 
+                    <input
+                        id='email'
+                        name='email'
+                        type='text'
                         className='input_auth fonts-roboto-light'
-                        placeholder='Введите логин'/>
-                    <input type='password' 
-                            className='input_auth fonts-roboto-light'
-                            placeholder='Введите пароль'/>    
-                    
+                        placeholder='Введите логин'
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                    />
+                    {formik.touched.email && formik.errors.email ? (
+                        <div className='error-massage'>{formik.errors.email}</div>
+                    ) : null}
+                    <input
+                        id='password'
+                        name='password'
+                        type='password'
+                        className='input_auth fonts-roboto-light'
+                        placeholder='Введите пароль'
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                    />
+                    {formik.touched.password && formik.errors.password ? (
+                        <div className='error-massage'>{formik.errors.password}</div>
+                    ) : null}
+                    {error !== '' ? (
+                        <div className='error-massage'>Возникла ошибка. Введите корректные данные для входа.</div>
+                    ) : null}
                     <div className='form-button'>
                     <Button parametr={'Войти'} type={'submit'}/>
                         <p className='fonts-roboto-regular clue'>Еще нет аккаунта? <a href='/registration' className='link'>Регистрация</a></p>
--- src/components/authorization/RegistrationForm.js
+++ src/components/authorization/RegistrationForm.js
@@ -2,109 +2,221 @@ import './authorization.css'
 import '../../fonts/roboto/fonts.css'
 import Button from '../button/Button'
 import axios from '../../utils/axios'
-import { useContext, useState, useEffect } from 'react'
-import { CustomContext } from '../../utils/Context'
-import { useNavigate } from 'react-router'
+import {useContext, useState, useEffect} from 'react'
+import {CustomContext} from '../../utils/Context'
+import {useNavigate} from 'react-router'
+import {useFormik} from "formik";
+import * as Yup from "yup";
 
 const RegistrationForm = () => {
 
     const {user, setUser} = useContext(CustomContext)
     const [sportsTitle, setSportsTitles] = useState([])
     const [regions, setRegions] = useState([])
+    const [sexes, setSexes] = useState([])
 
     const navigate = useNavigate()
     useEffect(() => {
         axios.get('general/allSportsTitle')
-        .then(({data}) => setSportsTitles(data))
+            .then(({data}) => setSportsTitles(data))
         axios.get('general/allRegions')
-        .then(({data}) => setRegions(data))
-        }, []);
+            .then(({data}) => setRegions(data))
+        axios.get('general/allSex')
+            .then(({data}) => setSexes(data))
+    }, []);
 
-    const registerUser = (e) => {
-        e.preventDefault()
-        console.log(e.target[3].value)
-       
-        let newUser = {
-            'firstName':e.target[0].value,
-            'surname':e.target[1].value,
-            'patronymic':e.target[2].value,
-            'email':e.target[7].value,
-            'password':e.target[8].value,
-            'region':{
-                'id':e.target[4].value
-            },
-            'sex':{
-                'id':e.target[5].value
-            },
-            'sportsTitle': {
-                'id':e.target[6].value
-            },
-            'birthDate':e.target[3].value
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            surname: '',
+            patronymic: '',
+            email: '',
+            password: '',
+            region: '',
+            sex: '',
+            sportsTitle: '',
+            birthDate: '',
+        },
+        validationSchema: Yup.object({
+            firstName: Yup.string()
+                .required('Поле обязательно для заполнения'),
+            surname: Yup.string()
+                .required('Поле обязательно для заполнения'),
+            email: Yup.string().email("Поле не удовлетворяет требованиям"),
+            password: Yup.string()
+                .required('Поле обязательно для заполнения'),
+            region: Yup.string()
+                .required('Поле обязательно для заполнения'),
+            sex: Yup.string()
+                .required('Поле обязательно для заполнения'),
+            birthDate: Yup.string()
+                .required('Поле обязательно для заполнения'),
+        }),
+        onSubmit: async values => {
+            let newUser = {
+                'firstName': values.firstName,
+                'surname': values.surname,
+                'patronymic': values.patronymic,
+                'email': values.email,
+                'password': values.password,
+                'region': {
+                    'id': values.region
+                },
+                'sex': {
+                    'id': values.sex
+                },
+                'sportsTitle': {
+                    'id': values.sportsTitle
+                },
+                'birthDate': values.birthDate
 
+            }
+
+            console.log(newUser)
+
+            axios.post('auth/signup', newUser)
+                .then(({data}) => {
+                    setUser({
+                        ...data
+                    })
+                    localStorage.setItem('user', JSON.stringify({
+                        ...data
+                    }))
+                    navigate('/login')
+                })
+                .catch((err) => console.log(err.message))
         }
+    })
 
-        console.log(newUser)
 
-        axios.post('auth/signup', newUser)
-                .then(({data}) => 
-            { 
-                setUser({ 
-                    ...data 
-                }) 
-                localStorage.setItem('user', JSON.stringify({ 
-                    ...data 
-                })) 
-                navigate('/login') 
-            }) 
-            .catch((err) => console.log(err.message))
-    }
-
-    
-
-    return(
+    return (
         <div className='authorization'>
             <div className='popup'>
-                <form className='form' onSubmit={registerUser}>
+                <form className='form' onSubmit={formik.handleSubmit}>
                     <p className='header fonts-roboto-black'>Регистрация</p>
-                    <input type='text' 
-                            className='input_auth fonts-roboto-light'
-                            placeholder='Введите имя'/>
-                    <input type='text' 
-                            className='input_auth fonts-roboto-light'
-                            placeholder='Введите фамилию'/>
-                    <input type='text' 
-                            className='input_auth fonts-roboto-light'
-                            placeholder='Введите отчество'/>
-                    <input type='date' 
-                            className='input_auth fonts-roboto-light'
-                            placeholder='Введите отчество'/>
+                    <input
+                        id='firstName'
+                        name='firstName'
+                        type='text'
+                        className='input_auth fonts-roboto-light'
+                        placeholder='Введите имя'
+                        value={formik.values.firstName}
+                        onChange={formik.handleChange}
+                    />
+                    {formik.touched.firstName && formik.errors.firstName ? (
+                        <div className='error-massage'>{formik.errors.firstName}</div>
+                    ) : null}
 
-                    <select className='input_auth fonts-roboto-light'>
-                                <option value='' disabled selected hidden>Выберите регион</option>
-                                {regions.map(region => (
-                                    <option value={region?.id}>{region?.name}</option>
-                                ))}
+                    <input
+                        id='surname'
+                        name='surname'
+                        type='text'
+                        className='input_auth fonts-roboto-light'
+                        placeholder='Введите фамилию'
+                        value={formik.values.surname}
+                        onChange={formik.handleChange}
+                    />
+                    {formik.touched.surname && formik.errors.surname ? (
+                        <div className='error-massage'>{formik.errors.surname}</div>
+                    ) : null}
+
+                    <input
+                        id='patronymic'
+                        name='patronymic'
+                        type='text'
+                        className='input_auth fonts-roboto-light'
+                        placeholder='Введите отчество'
+                        value={formik.values.patronymic}
+                        onChange={formik.handleChange}
+                    />
+
+                    <input
+                        id='birthDate'
+                        name='birthDate'
+                        type='date'
+                        className='input_auth fonts-roboto-light'
+                        placeholder='Введите дату рождения'
+                        value={formik.values.birthDate}
+                        onChange={formik.handleChange}
+                    />
+                    {formik.touched.birthDate && formik.errors.birthDate ? (
+                        <div className='error-massage'>{formik.errors.birthDate}</div>
+                    ) : null}
+
+                    <select
+                        id='region'
+                        name='region'
+                        className='input_auth fonts-roboto-light'
+                        value={formik.values.region}
+                        onChange={formik.handleChange}>
+                        <option value='' disabled selected hidden>Выберите регион</option>
+                        {regions.map(region => (
+                            <option value={region?.id}>{region?.name}</option>
+                        ))}
                     </select>
-                    <select className='input_auth fonts-roboto-light'>
+                    {formik.touched.region && formik.errors.region ? (
+                        <div className='error-massage'>{formik.errors.region}</div>
+                    ) : null}
+
+                    <select
+                        id='sex'
+                        name='sex'
+                        className='input_auth fonts-roboto-light'
+                        value={formik.values.sex}
+                        onChange={formik.handleChange}>
                         <option value='' disabled selected hidden>Выберите пол</option>
-                        <option value='c99ccd51-5731-42a3-9cfc-31cc4011e035'>Мужской</option>
-                        <option value='2'>Женский</option>
+                        {sexes.map(sex => (
+                            <option value={sex?.id}>{sex?.name}</option>
+                        ))}
                     </select>
-                    <select className='input_auth fonts-roboto-light'>
-                                <option value='' disabled selected hidden>Выберите спортивный разряд</option>
-                                {sportsTitle.map(region => (
-                                    <option value={region?.id}>{region?.name}</option>
-                                ))}
+                    {formik.touched.sex && formik.errors.sex ? (
+                        <div className='error-massage'>{formik.errors.sex}</div>
+                    ) : null}
+
+                    <select
+                        id='sportsTitle'
+                        name='sportsTitle'
+                        className='input_auth fonts-roboto-light'
+                        value={formik.values.sportsTitle}
+                        onChange={formik.handleChange}>
+                        <option value='' disabled selected hidden>Выберите спортивный разряд</option>
+                        {sportsTitle.map(sportsTitle => (
+                            <option value={sportsTitle?.id}>{sportsTitle?.name}</option>
+                        ))}
                     </select>
-                    <input type='email' 
-                            className='input_auth fonts-roboto-light'
-                            placeholder='Введите email'/>
-                    <input type='password' 
-                            className='input_auth fonts-roboto-light'
-                            placeholder='Введите пароль'/>    
+                    {formik.touched.sportsTitle && formik.errors.sportsTitle ? (
+                        <div className='error-massage'>{formik.errors.sportsTitle}</div>
+                    ) : null}
+
+                    <input
+                        id='email'
+                        name='email'
+                        type='email'
+                        className='input_auth fonts-roboto-light'
+                        placeholder='Введите email'
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                    />
+                    {formik.touched.email && formik.errors.email ? (
+                        <div className='error-massage'>{formik.errors.email}</div>
+                    ) : null}
+
+                    <input
+                        id='password'
+                        name='password'
+                        type='password'
+                        className='input_auth fonts-roboto-light'
+                        placeholder='Введите пароль'
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                    />
+                    {formik.touched.password && formik.errors.password ? (
+                        <div className='error-massage'>{formik.errors.password}</div>
+                    ) : null}
+
                     <div className='form-button'>
                         <Button parametr={'Зарегистрироваться'} type={'submit'}/>
-                        <p className='fonts-roboto-regular clue'>Уже есть аккаунт? 
+                        <p className='fonts-roboto-regular clue'>Уже есть аккаунт?
                             <a href='/login' className='link'>Войти</a>
                         </p>
                         <p className='fonts-roboto-regular clue'>
